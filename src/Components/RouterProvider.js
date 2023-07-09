2da41326a7be7bd9935f25ecc8be0439d90a7193
sowmya059 <135099660+sowmya059@users.noreply.github.com>
2023-07-09 15:14:36 +0530

    Update RouterProvider.js

--- src/Components/RouterProvider.js
+++ src/Components/RouterProvider.js
@@ -1,14 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
-import Cart from './Cart'
-import Home from './Home'
+import { Routes, Route } from "react-router-dom";
+import Cart from "./Cart";
+import Products from "./Products";
 
-const RouterProvider = () => {
+const RouterProvider = ({ cart, handleAddtoCart }) => {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/cart' element={<Cart />} />
-    </Routes>
-  )
-}
-
-export default RouterProvider
+    <>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
+};
+export default RouterProvider;
