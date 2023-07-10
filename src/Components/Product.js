4852476b7810854162f7dd0520acad28c65d8533
sowmya059 <sowmya.murali@dymensions.io>
2023-07-10 11:31:11 +0530

    updated files

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useContext } from "react";
 import "./Products.css";
 import { CartProduct } from "../storecontext/Context";
@@ -24,30 +23,6 @@ export const Product = ({ item }) => {
       setCartProducts(updatedCart);
     } else {
       setCartProducts((prev) => [
-=======
-import React, { useContext } from 'react'
-import { CartProduct } from '../storecontext/Context'
-
-export const Product = ({ item }) => {
-  const { title, image, price, category } = item
-
-  const { cartProducts, setCartProducts } = useContext(CartProduct)
-
-  const handleAddtoCart = item => {
-    const isProduct = cartProducts.findIndex(product => product.id === item.id)
-
-    if (isProduct !== -1) {
-      const updatedCartProducts = cartProducts.map((product, index) => {
-        if (index === isProduct) {
-          return { ...product, quantity: product.quantity + 1 }
-        }
-        return { ...product }
-      })
-
-      setCartProducts(updatedCartProducts)
-    } else {
-      setCartProducts(prev => [
->>>>>>> 27a2b71827e3cd51d909c723c9a414fdeb304ed8
         ...prev,
         {
           ...prev,
@@ -59,7 +34,6 @@ export const Product = ({ item }) => {
           rating: item.rating,
           quantity: 1,
         },
-<<<<<<< HEAD
       ]);
       console.log(cartProducts);
     }
@@ -84,25 +58,3 @@ export const Product = ({ item }) => {
     </>
   );
 };
-=======
-      ])
-      console.log(cartProducts)
-    }
-  }
-
-  return (
-    <div className='product'>
-      <img className='product-image' src={image} alt='card' />
-      <h5 className='product-name'>{title}</h5>
-      <h4>price:{price}</h4>
-      <h4>category:{category}</h4>
-
-      <div>
-        <button className='product-add' onClick={() => handleAddtoCart(item)}>
-          ADD TO CART
-        </button>
-      </div>
-    </div>
-  )
-}
->>>>>>> 27a2b71827e3cd51d909c723c9a414fdeb304ed8
--- src/storecontext/Context.js
+++ src/storecontext/Context.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createContext, useState } from "react";
 
 export const CartProduct = createContext();
@@ -19,58 +18,6 @@ const CartProvider = ({ children }) => {
       });
 
       setCartProducts(updatedCart);
-    } else {
-      setCartProducts((prevCart) => [...prevCart, { ...item, quantity: 1 }]);
-    }
-  };
-
-  const handleDecrease = (item) => {
-    const itemIndex = cartProducts.findIndex(
-      (cartItem) => cartItem.id === item.id
-    );
-
-    if (itemIndex !== -1) {
-      const updatedCart = [...cartProducts];
-      updatedCart[itemIndex].quantity -= 1;
-
-      if (updatedCart[itemIndex].quantity === 0) {
-        updatedCart.splice(itemIndex, 1);
-      }
-
-      setCartProducts(updatedCart);
-    }
-  };
-
-  return (
-    <CartProduct.Provider
-      value={{ cartProducts, setCartProducts, handleIncrease, handleDecrease }}
-    >
-      {children}
-    </CartProduct.Provider>
-  );
-};
-export default CartProvider;
-=======
-import { createContext, useState } from "react";
-
-export const CartProduct = createContext();
-const CartProvider = ({ children }) => {
-  const [cartProducts, setCartProducts] = useState([]);
-
-  const handleIncrease = (item) => {
-    const isProduct = cartProducts.findIndex(
-      (product) => product.id === item.id
-    );
-
-    if (isProduct !== -1) {
-      const updatedCartProducts = cartProducts.map((product, index) => {
-        if (index === isProduct) {
-          return { ...product, quantity: product.quantity + 1 };
-        }
-        return { ...product };
-      });
-
-      setCartProducts(updatedCartProducts);
     } else {
       setCartProducts((prevCart) => [...prevCart, { ...item, quantity: 1 }]);
     }
@@ -102,4 +49,3 @@ const CartProvider = ({ children }) => {
   );
 };
 export default CartProvider;
->>>>>>> 27a2b71827e3cd51d909c723c9a414fdeb304ed8
